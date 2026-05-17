75567b94c0714d8ebf3dd75f8e1082ddb311e81d
vrushalijagtap43 <vrushalijagtap43@gmail.com>
2026-05-17 18:51:49 +0530

    postbook done

--- sharpapp/views.py
+++ sharpapp/views.py
@@ -49,6 +49,14 @@ class Bookcrud(APIView):
 
         return Response(serializers.data,status=status.HTTP_200_OK)
 
+    def post(self,request):
+        serializer=BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
 
 
 
